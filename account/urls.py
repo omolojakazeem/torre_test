ea0ee45a1ba95ac30bd4a7c127c2f1b3c66d4d29
Kazeem Omoloja <omolojakazeem@gmail.com>
2021-11-24 23:40:15 +0000

    Finetune url

--- account/urls.py
+++ account/urls.py
@@ -5,8 +5,8 @@ app_name = 'auth'
 
 urlpatterns = [
     path('auth/', views.Authentication.as_view(), name='auth_user'),
-    path('dashboard/<int:pk>', views.Dashboard.as_view(), name='dashboard'),
-    path('mydetail/<int:pk>', views.DashboardUserApi.as_view(), name='user_list_api'),
-    path('related/<str:skill>', views.RelatedUserApi.as_view(), name='related_user_list_api'),
-    path('all/', views.AllUserApi.as_view(), name='user_list'),
+    path('dashboard/<int:pk>/', views.Dashboard.as_view(), name='dashboard'),
+    path('mydetail/<int:pk>/', views.DashboardUserApi.as_view(), name='user_list_api'),
+    path('related/<str:skill>/', views.RelatedUserApi.as_view(), name='related_user_list_api'),
+    path('', views.AllUserApi.as_view(), name='user_list'),
 ]
--- config/urls.py
+++ config/urls.py
@@ -39,7 +39,7 @@ schema_view = get_schema_view(
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('__debug__/', include(debug_toolbar.urls)),
-    path('account/', include("account.urls")),
+    path('', include("account.urls")),
     path(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
     path('documentation/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     path(r'^redoc/$', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
